36731041e4645bc270ece435d43439c0c503c014
msrother <95832683+msrother@users.noreply.github.com>
2022-06-19 18:45:23 -0300

    novo arquivo
    Arquivo script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function Criar() {
+
+    var nome = document.getElementById("nome");
+
+    if (nome.value != "") {
+        alert('Prezado(a) ' + nome.value + ', sua conta foi criada com sucesso.');
+    } 
+        else {
+            alert('Nome não informado!')
+
+        }
+    
+
+}
+
+
 let btn = document.querySelector('.lnr-eye');
 btn.addEventListener('click', function() {
     let input = document.querySelector('#password');
