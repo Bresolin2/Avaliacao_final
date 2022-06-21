4903da2e5ba078ac923da33727f6201bfe61e4e5
Lucas Bresolin <lucas.bresolin2248@gmail.com>
2022-06-21 20:53:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-function Criar() {
+// function Criar() {
 
-    var nome = document.getElementById("nome");
+//     var nome = document.getElementById("nome");
        
 
-    if (nome.value != "") {
-        alert('Prezado(a) ' + nome.value + ', sua conta foi criada com sucesso.');   
-    } 
-        else {
+//     if (nome.value != "") {
+//         alert('Prezado(a) ' + nome.value + ', sua conta foi criada com sucesso.');   
+//     } 
+//         else {
             
-            alert('Por favor preencha todos os campos!')     
-        }
+//             alert('Por favor preencha todos os campos!')     
+//         }
        
-}
+// }
 
+
+const express = require('express');
+const session = require('express-session');
+
+const port = 3000;
+var path = require('path');
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+
+app.listen(port,()=> {
+    console.log('servidor rodando');
+})
