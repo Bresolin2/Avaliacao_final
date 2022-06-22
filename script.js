53bb7344d26f38f3c643f55b2f46092364f0ec18
Lucas Bresolin <lucas.bresolin2248@gmail.com>
2022-06-21 21:54:10 -0300

    taporra

--- script.js
+++ script.js
@@ -16,15 +16,28 @@
 
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const port = 3000;
 var path = require('path');
 const app = express();
 
+app.use(session({secret:'secret'}));
+
 app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
+
+app.post('/', (req, res)=> {
+    console.log(req.body.login);
+    res.render('../html/Login.html')
+})
+
+app.get('/', (req, res)=> {
+    res.render('../html/Login.html')
+})
 
 
 app.listen(port,()=> {
