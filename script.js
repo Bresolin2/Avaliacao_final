c0e72b8d0422784b1ba084d96c8908dbf9a3a505
msrother <95832683+msrother@users.noreply.github.com>
2022-06-21 23:13:56 -0300

    função criar script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 
-    //  var nome = document.getElementById("nome");
-    //  var email = document.getElementById("email");
-    //  var senha = document.getElementById("password");      
-       
-    //  if (nome.value && email.value && senha.value != "") {
-    //      alert('Prezado(a) ' + nome.value + ', sua conta foi criada com sucesso.');   
-    //  } 
-    //      else {           
-    //          alert('Por favor preencha todos os campos!')     
-    //      }
+// função botão criar do cadastro
 
+function Criar() {
+
+    var nome = document.getElementById("nome");
+    var email = document.getElementById("email");
+    var senha = document.getElementById("password");      
+        
+     if (nome.value && email.value && senha.value != "") {
+         alert('Prezado(a) ' + nome.value + ', sua conta foi criada com sucesso.');   
+       } 
+           else {           
+               alert('Por favor preencha todos os campos!')     
+           }
+}
+
+// fim da função botão criar
+//-------------------------------------   
 
 
 
