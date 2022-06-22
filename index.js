c59f2ca9f16b296612f92a8841fa2ce40826a32c
msrother <95832683+msrother@users.noreply.github.com>
2022-06-21 23:48:06 -0300

    excluído scritp.js
    modificado o js

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ $(document).ready(function(){
         }
     });
 });
+
+
+
+// função botão criar do cadastro
+
+function Criar() {
+
+    var nome = document.getElementById("nome");
+    var email = document.getElementById("email");
+    var senha = document.getElementById("password");      
+        
+     if (nome.value && email.value && senha.value != "") {
+         alert('Prezado(a) ' + nome.value + ', sua conta foi criada com sucesso.');   
+       } 
+           else {           
+               alert('Por favor preencha todos os campos!')     
+           }
+}
+
+// fim da função botão criar
+//-------------------------------------   
